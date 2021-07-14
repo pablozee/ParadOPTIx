c0ab5ca6986beb08298f857cb339ee773c686830
pablozee <zeshanrasul@gmail.com>
2021-07-14 18:40:41 +0100

    Launch Params Struct

---
+++ ParadOPTIx/LaunchParams.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "include/gdt/gdt/math/vec.h"
+
+namespace ParadOPTIx {
+	using namespace gdt;
+
+	struct LaunchParams
+	{
+		int			frameID{ 0 };
+		uint32_t*	colorBuffer;
+		vec2i		fbSize;
+	};
+}
