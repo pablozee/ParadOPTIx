8af33f617f04796500e6100570b4612666eb4715
pablozee <zeshanrasul@gmail.com>
2021-07-15 10:24:33 +0100

    DeviceProrgams CUDA file and Main.cpp - Example 2

---
+++ ParadOPTIx/devicePrograms.cu
@@ -0,0 +1,67 @@
+#include <optix_device.h>
+
+#include "LaunchParams.h"
+
+using namespace ParadOPTIx;
+
+namespace ParadOPTIx {
+
+	/*!
+		Launch parameters in constant memory, filled in by optix upon
+		optixLaunch. This gets filled in from the buffer we pass to optixLaunch,
+		passed from host to device
+	*/
+	extern "C" __constant__ LaunchParams optixLaunchParams;
+
+	// Closest hit and anyhit programs for radiance-type rays
+	// Will eventually need one pair for each ray type and each geometry type
+	// we want to render
+
+	extern "C" __global__ void __closesthit_radiance()
+	{}
+
+	extern "C" __global__ void __anyhit_radiance()
+	{}
+
+	// Miss program that gets called for any ray that did not have a
+	// valid intersection
+
+	extern "C" __global__ void __miss_radiance()
+	{}
+
+	// Ray gen program - the actual rendering happens here
+	extern "C" __global__ void __raygen__renderFrame()
+	{
+		if (optixLaunchParams.frameID == 0 &&
+			optixGetLaunchIndex().x   == 0 &&
+			optixGetLaunchIndex().y   == 0)
+		{
+			/*!
+			* We use the optixLaunchParams rather than querying the optixGetLaunchDims
+			* to ensure the optixLaunchParams are not being optimized away when not 
+			* being used
+			*/
+			printf("############################################\n");
+			printf("Hello world from OptiX 7 raygen program!\n(within a %ix%i-sized launch)\n",
+				optixLaunchParams.fbSize.x,
+				optixLaunchParams.fbSize.y);
+			printf("############################################\n");
+		}
+
+		// Compute a test pattern based on pixel ID
+		const int ix = optixGetLaunchIndex().x;
+		const int iy = optixGetLaunchIndex().y;
+
+		const int r = (ix % 256);
+		const int g = (iy % 256);
+		const int b = ((ix + iy) % 256);
+
+		// Convert to 32-bit rgba value setting alpha to 0xff
+		const uint32_t rgba = 0xff000000 | (r << 0) | (g << 8) | (b << 16);
+
+		// Write to frame buffer
+		const uint32_t fbIndex = ix + iy * optixLaunchParams.fbSize.x;
+		optixLaunchParams.colorBuffer[fbIndex] = rgba;
+	}
+
+}
--- ParadOPTIx/main.cpp
+++ ParadOPTIx/main.cpp
@@ -1,42 +1,32 @@
-#include "include/gdt/gdt/gdt.h"
-#include "optix7.h"
+#include "Renderer.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "include/3rdParty/stb_image_write.h"
 
 namespace ParadOPTIx {
-
-	// Helper function that initializes optix and checks for errors
-	void initOptix()
-	{
-
-		// Check for available optix7 capable devices
-
-		cudaFree(0);
-
-		int numDevices;
-		cudaGetDeviceCount(&numDevices);
-
-		if (numDevices == 0)
-			throw std::runtime_error("#ParadOPTIx: no CUDA capable devices found!");
-
-		std::cout << "#ParadOPTIx: found " << numDevices << " CUDA devices" << std::endl;
-
-		// Initialize Optix
-		OPTIX_CHECK( optixInit() );
-	}
 
 	// main entry point - initialize optix, print hello world, then exit
 	extern "C" int main(int ac, char** av)
 	{
 		try {
-			std::cout << "#ParadOPTIx: initializing Optix..." << std::endl;
+			Renderer renderer;
 
-			initOptix();
+			const vec2i fbSize(vec2i(1200, 1024));
+			renderer.resize(fbSize);
+			renderer.render();
+
+			std::vector<uint32_t> pixels(fbSize.x * fbSize.y);
+			renderer.downloadPixels(pixels.data());
+
+			const std::string fileName = "ParadOPTIx_example2.png";
+			stbi_write_png(fileName.c_str(), fbSize.x, fbSize.y, 4,
+				pixels.data(), fbSize.x * sizeof(uint32_t));
 
 			std::cout << GDT_TERMINAL_GREEN
-					  << "#ParadOPTIx: Successfully initialized Optix"
-					  << GDT_TERMINAL_DEFAULT << std::endl;
-
-			// For this simple hello world example, don't do anything else
-			std::cout << "#ParadOPTIx: Done. Clean exit" << std::endl;
+					  << std::endl
+					  << "Image rendered, and saved to " << fileName << " ... done." << std::endl
+					  << GDT_TERMINAL_DEFAULT 
+					  << std::endl;
 		}
 		catch (std::runtime_error& e) {
 			std::cout << GDT_TERMINAL_RED << "FATAL ERROR: " << e.what()
